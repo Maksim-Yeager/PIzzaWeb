8907f9bdd24f18defabcc0bf1dd2b38c01d6dd7c
Maksim-Yeager <maksim12345daniel@gmail.com>
2023-03-20 09:58:57 +0200

    The Program

---
+++ RazorPizzeria/RazorPizzeria/Pages/Checkout/ThankYou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages.Check_out
+{
+    public class ThankYouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
